5ae3dea50e42113af8c4cf81e8f472513694b415
Gianpaolo <gian.impronta@gmail.com>
2021-08-30 10:49:28 -0300

    Commit inicial

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,8 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		nome: "Gian",
+		idade: 29,
+		imgSrc: 'https://www.google.com/logos/doodles/2021/get-vaccinated-wear-a-mask-save-lives-august-4-6753651837109295-law.gif'
+	}
+	})
