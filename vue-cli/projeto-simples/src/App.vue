485e9de137be32b2f2d22b5b5fdbbad72156874f
Gianpaolo <gian.impronta@gmail.com>
2021-09-15 16:48:13 -0300

    Usando vue-cli

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+    <button @click='titulo += "#"'>alterar</button>
+  </div>
+</template>
+<script>
+export default {
+  data: function(){
+    return {
+      titulo: "Teste data usando Vue"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  background-color: cadetblue;
+  color: whitesmoke
+}
+</style>
